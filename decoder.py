4e0a8260b16b38f4edd19372664217c41e6bca7a
Dmitry Ponyatov <dponyatov@gmail.com>
2014-09-30 18:00:30 +0400

    * сигнатура + разбор битовых полей (гемор) + классы полей кадра * Flame()
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- decoder.py
+++ decoder.py
@@ -83,7 +83,7 @@ class ByteStream:
         'перестроение индекса пакетов'
         self.INDEX=[]
         for i in range(len(self.DAT)-31):
-            if self.DAT[i:i+2]==[0x55,0xAA]:
+            if self.DAT[i:i+3]==[0x00,0x55,0xAA]:
                 if self.isOkpacket(i):
                     self.INDEX+=[i]
     def isOkpacket(self,addr):
@@ -104,26 +104,92 @@ K1=ByteStream('K1',CHBT['K1']) ; print K1
 K2=ByteStream('K2',CHBT['K2']) ; print K2
 K3=ByteStream('K3',CHBT['K3']) ; print K3
 
-############################
+############# вспомогательные функции ###############
 
-class Package:
+def BF(dat,base,bitdef):
+    'разбор битовых полей'
+    N=0
+    for i in bitdef:
+        BYTE,BITS=i ; BYTE-=base
+        for b in range(BITS[0],BITS[1]+1):
+            N=(N<<1)|(dat[BYTE]>>(8-b))&1
+    return N
+
+def HD(dat):
+    'hexdump списка'
+    return '[%s]'%(\
+        reduce(lambda a,b:'%s %s'%(a,b),\
+               map(lambda x:'%.2X'%x,dat)))
+
+############# классы полей кадра ###############
+
+class Signatura:
+    'Заголовок'
+    def __init__(self,dat):
+        assert len(dat)==3
+        self.dat=dat
+    def __str__(self):
+        A,B,C=self.dat 
+        return HD(self.dat)
+    
+class AnyTime:
+    'метка времени'
+    def __init__(self,dat):
+        assert len(dat)==4
+        self.DAT=dat
+        self.SEC= BF(dat,4,[(4,(0,5))])
+        self.MIN= BF(dat,4,[(4,(6,7)),(5,(0,3))])
+        self.HOUR=BF(dat,4,[(5,(4,7)),(6,(0,0))])
+        self.DAYS=BF(dat,4,[(6,(1,7)),(7,(0,7))])
+    def __str__(self): return '%s\t\t%.2i:%.2i:%.2i:%.2i'%(HD(self.DAT),\
+            self.DAYS,self.HOUR,self.MIN,self.SEC)
+class ShtyrTime(AnyTime):
+    'Время Штиля'
+class BSKVU(AnyTime):
+    'Фремя БСКВУ'
+
+class PeakDM:
+    'пик ДМ'
+    def __init__(self,dat):
+        assert len(dat)==5
+        self.DAT=dat
+        self.X =BF(dat,16,[(16,(0,7)),(17,(0,3))])
+        self.Y =BF(dat,16,[(17,(4,7)),(18,(0,7))])
+        self.Z =BF(dat,16,[(19,(0,7)),(20,(0,7))])
+    def __str__(self): return '%s\tX:%i Y:%i Z:%i'%(HD(self.DAT),\
+            self.X,self.Y,self.Z)
+
+class Termo:
+    'Температура'
+    def __init__(self,dat):
+        assert len(dat)==4
+        self.DAT=dat
+        self.DM1 =BF(dat,26,[(26,(0,7)),(27,(0,3))])
+        self.DM2 =BF(dat,26,[(27,(4,7)),(28,(0,7))])
+        self.SHT =BF(dat,26,[(29,(0,7))])
+    def __str__(self): return '%s\tDM1:%i DM2:%i SHT:%i'%(HD(self.DAT),\
+            self.DM1,self.DM2,self.SHT)
+
+############# класс кадра ###############
+
+class Frame:
     'пакет'
     def __init__(self,ch,addr,block):
         assert len(block)==32
         self.CH=ch
         self.ADDR=addr
         self.DAT=block
-        # декодирование
-        self.signature=self.DAT[0:2+1]
-        self.blockN=self.DAT[3]
-        self.time=self.DAT[4:7+1]
-        self.BSKVU1=self.DAT[8:11+1]
-        self.BSKVU2=self.DAT[12:15+1]
-        self.DM1peak=self.DAT[16:20+1]
-        self.DM2peak=self.DAT[21:25+1]
-        self.Temp=self.DAT[26:29+1]
-        self.CRC_H=self.DAT[30]
-        self.CRC_L=self.DAT[31]
+        # декодирование с выделением срезов из списка байт 
+        self.signature  =Signatura( self.DAT[0:2+1] )
+        self.blockN     =           self.DAT[3]
+        self.time       =ShtyrTime( self.DAT[4:7+1])
+        self.BSKVU1     =BSKVU(     self.DAT[8:11+1])
+        self.BSKVU2     =BSKVU(     self.DAT[12:15+1])
+        self.DM1peak    =PeakDM(    self.DAT[16:20+1])
+        self.DM2peak    =PeakDM(    self.DAT[21:25+1])
+        self.Temp       =Termo(     self.DAT[26:29+1])
+        self.CRC_H      =           self.DAT[30]
+        self.CRC_L      =           self.DAT[31]
     def checksum(self,addr): return sum(self.DAT[0,29+1])
     def __str__(self):
         HL='-'*55
@@ -131,19 +197,19 @@ class Package:
         T+=dump(self.DAT)
         T+='\n'+HL
         T+='\nсигнатура:\t\t%s'%self.signature
-        T+='\n№ блока:\t\t%s'%self.blockN
+        T+='\n№ блока:\t\t%.2X'%self.blockN
         T+='\nвремя Штыря:\t%s'%self.time
         T+='\nБСКВУ1:\t\t\t%s'%self.BSKVU1
         T+='\nБСКВУ2:\t\t\t%s'%self.BSKVU2
         T+='\nПик DM1:\t\t%s'%self.DM1peak
         T+='\nПик DM2:\t\t%s'%self.DM2peak
         T+='\nТемпература:\t%s'%self.Temp
-        T+='\nCRC_H:\t\t\t%s'%self.CRC_H
-        T+='\nCLC_L:\t\t\t%s'%self.CRC_L
+        T+='\nCRC_H:\t\t\t%.2X'%self.CRC_H
+        T+='\nCLC_L:\t\t\t%.2X'%self.CRC_L
         T+='\n'+HL
         return T
 
 for K in [K1,K2,K3]:
     for P in K.packages():
         print
-        print Package(K.ID,P,K.package(P))
+        print Frame(K.ID,P,K.package(P))
